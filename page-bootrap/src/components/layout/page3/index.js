eaf4387421b33865b603576f16406f68c7ce1fcb
thieuem2003 <142884364+Thieuem2003@users.noreply.github.com>
2023-08-23 21:38:14 +0700

    thieu

--- page-bootrap/src/components/layout/page3/index.js
+++ page-bootrap/src/components/layout/page3/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import "../page3/style.css";
 export default function page2() {
     return (
         <>
@@ -7,48 +7,48 @@ export default function page2() {
             <div class="row">
               <div class="col-md-2">
                 <div class="card">
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="iPhone"></img>
                   <div class="card-body">
+                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="iPhone"></img>
                     <h5 class="card-title">iPhone</h5>
                   </div>
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="card">
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Mac"></img>
                   <div class="card-body">
+                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Mac"></img>
                     <h5 class="card-title">Mac</h5>
                   </div>
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="card">
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="iPad"></img>
                   <div class="card-body">
+                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="iPad"></img>
                     <h5 class="card-title">iPad</h5>
                   </div>
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="card">
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Watch"></img>
                   <div class="card-body">
+                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Watch"></img>
                     <h5 class="card-title">Watch</h5>
                   </div>
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="card">
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Âm Thanh"></img>
                   <div class="card-body">
+                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Âm Thanh"></img>
                     <h5 class="card-title">Âm Thanh</h5>
                   </div>
                 </div>
               </div>
               <div class="col-md-2">
                 <div class="card">
-                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Phụ Kiện"></img>
                   <div class="card-body">
+                  <img src="https://via.placeholder.com/150" class="card-img-top" alt="Phụ Kiện"></img>
                     <h5 class="card-title">Phụ Kiện</h5>
                   </div>
                 </div>
