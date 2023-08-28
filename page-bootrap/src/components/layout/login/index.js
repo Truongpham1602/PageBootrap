af8ac0a1812c71e614483fe38f1665e397279d6c
PhamThanh <phamvanthanh728@gmail.com>
2023-08-28 21:48:05 +0700

    update
    Co-Authored-By: Truongpham1602 <73901586+Truongpham1602@users.noreply.github.com>

--- page-bootrap/src/components/layout/login/index.js
+++ page-bootrap/src/components/layout/login/index.js
@@ -1,65 +1,69 @@
-import { React } from 'react';
+import React from 'react';
 import './css/main.css';
 import './css/util.css';
 import './fonts/font-awesome-4.7.0/css/font-awesome.min.css'
+import { Link } from 'react-router-dom';
 
-export default function login() {
+class login extends React.Component {
 
-    return (
-        <>
-            <div className="limiter">
-                <div className="container-login100">
-                    <div className="wrap-login100">
-                        <div className="login100-pic js-tilt" data-tilt>
-                            <img src={require('./images/img-01.png')} alt="IMG" />
+    render(){
+        return (
+            <>
+                <div className="limiter">
+                    <div className="container-login100">
+                        <div className="wrap-login100">
+                            <div className="login100-pic js-tilt" data-tilt>
+                                <img src={require('./images/img-01.png')} alt="IMG" />
+                            </div>
+
+                            <form className="login100-form validate-form">
+                                <span className="login100-form-title">
+                                    Member Login
+                                </span>
+
+                                <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                                    <input className="input100" type="text" name="email" placeholder="Email" />
+                                    <span className="focus-input100"></span>
+                                    <span className="symbol-input100">
+                                        <i className="fa fa-envelope" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+
+                                <div className="wrap-input100 validate-input" data-validate = "Password is required">
+                                    <input className="input100" type="password" name="pass" placeholder="Password" />
+                                    <span className="focus-input100"></span>
+                                    <span className="symbol-input100">
+                                        <i className="fa fa-lock" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                                
+                                <div className="container-login100-form-btn">
+                                    <button className="login100-form-btn">
+                                        Login
+                                    </button>
+                                </div>
+
+                                <div className="text-center p-t-12">
+                                    <span className="txt1">
+                                    &#32;Forgot&#32;
+                                    </span> 
+                                    <a className="txt2" href="#">
+                                        Username / Password?
+                                    </a>
+                                </div>
+
+                                <div className="text-center p-t-136">
+                                    <Link className="txt2" to="/register">
+                                        Create your Account
+                                        <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                                    </Link>
+                                </div>
+                            </form>
                         </div>
-
-                        <form className="login100-form validate-form">
-                            <span className="login100-form-title">
-                                Member Login
-                            </span>
-
-                            <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                                <input className="input100" type="text" name="email" placeholder="Email" />
-                                <span className="focus-input100"></span>
-                                <span className="symbol-input100">
-                                    <i className="fa fa-envelope" aria-hidden="true"></i>
-                                </span>
-                            </div>
-
-                            <div className="wrap-input100 validate-input" data-validate = "Password is required">
-                                <input className="input100" type="password" name="pass" placeholder="Password" />
-                                <span className="focus-input100"></span>
-                                <span className="symbol-input100">
-                                    <i className="fa fa-lock" aria-hidden="true"></i>
-                                </span>
-                            </div>
-                            
-                            <div className="container-login100-form-btn">
-                                <button className="login100-form-btn">
-                                    Login
-                                </button>
-                            </div>
-
-                            <div className="text-center p-t-12">
-                                <span className="txt1">
-                                &#32;Forgot&#32;
-                                </span> 
-                                <a className="txt2" href="#">
-                                    Username / Password?
-                                </a>
-                            </div>
-
-                            <div className="text-center p-t-136">
-                                <a className="txt2" href="#">
-                                    Create your Account
-                                    <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-                                </a>
-                            </div>
-                        </form>
                     </div>
                 </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
 }
+export default login;
