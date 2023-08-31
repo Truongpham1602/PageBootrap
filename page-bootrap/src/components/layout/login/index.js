b432a2015e2e9373a20a6f57d77add262d7525cb
PhamThanh <33304485+NSVN-PhamVanThanh@users.noreply.github.com>
2023-08-31 11:31:54 +0700

    update

--- page-bootrap/src/components/layout/login/index.js
+++ page-bootrap/src/components/layout/login/index.js
@@ -6,8 +6,42 @@ import { Link } from 'react-router-dom';
 import { Tilt } from 'react-tilt';
 import $ from 'jquery';
 import 'tilt.js';
+import { DOMAIN } from '../../../util/url.constant';
 
 class login extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            "userName": "",
+            "password": ""
+        }
+    }
+
+    setParams = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    login = () => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+            "userName": this.state.userName,
+            "password": this.state.password
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch(`${DOMAIN}poseidon/public/api/v1/auth/authenticate`, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
     componentDidMount(){
         $('.js-tilt').tilt({
 			scale: 1.1
@@ -30,7 +64,7 @@ class login extends React.Component {
                                 </span>
 
                                 <div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-                                    <input className="input100" type="text" name="email" placeholder="Email" />
+                                    <input className="input100" type="text" name="userName" placeholder="User Name" onChange={this.setParams}/>
                                     <span className="focus-input100"></span>
                                     <span className="symbol-input100">
                                         <i className="fa fa-envelope" aria-hidden="true"></i>
@@ -38,7 +72,7 @@ class login extends React.Component {
                                 </div>
 
                                 <div className="wrap-input100 validate-input" data-validate = "Password is required">
-                                    <input className="input100" type="password" name="pass" placeholder="Password" />
+                                    <input className="input100" type="password" name="password" placeholder="Password" onChange={this.setParams}/>
                                     <span className="focus-input100"></span>
                                     <span className="symbol-input100">
                                         <i className="fa fa-lock" aria-hidden="true"></i>
@@ -46,7 +80,7 @@ class login extends React.Component {
                                 </div>
                                 
                                 <div className="container-login100-form-btn">
-                                    <button className="login100-form-btn">
+                                    <button type="button" className="login100-form-btn" onClick={this.login}>
                                         Login
                                     </button>
                                 </div>
@@ -60,7 +94,7 @@ class login extends React.Component {
                                     </a>
                                 </div>
 
-                                <div className="text-center p-t-136">
+                                <div className="text-center p-t-20">
                                     <Link className="txt2" to="/register">
                                         Create your Account
                                         <i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
