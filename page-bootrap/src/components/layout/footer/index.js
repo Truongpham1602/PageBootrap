3ee9bb616b800c0f0c8aba5d68b340c2bd911656
Thieuem2003 <142884364+Thieuem2003@users.noreply.github.com>
2023-09-26 20:57:06 +0700

    thieu

---
+++ page-bootrap/src/components/layout/footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer_001 from './footer001';
+import Footer_002 from './footer002';
+
+
+export default function Footer() {
+    return (
+        <>
+             <Footer_001/>
+             <Footer_002/>
+        </>
+    )
+}
